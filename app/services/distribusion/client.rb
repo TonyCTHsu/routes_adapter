e62a3f52c2f27adffb482a420b5fe148aafa8ebe
Tony Hsu <tonyc.t.hsu@gmail.com>
2019-04-02 21:35:58 +0800

    Rescue timeout and retry for client

--- app/services/distribusion/client.rb
+++ app/services/distribusion/client.rb
@@ -4,23 +4,36 @@ module Distribusion
       @conn = Faraday.new(url: url) do |faraday|
         faraday.request  :multipart
         faraday.request  :url_encoded
-        faraday.response :logger
+        faraday.response :logger if Rails.env.development?
         faraday.adapter Faraday.default_adapter
       end
     end
 
     def get(path, params)
-      @conn.get(path, params.merge(passphrase: passphrase))
+      with_timeout_retry do
+        @conn.get(path, params.merge(passphrase: passphrase))
+      end
     end
 
     def post(path, data)
-      @conn.post(path, data.merge(passphrase: passphrase))
+      with_timeout_retry do
+        @conn.post(path, data.merge(passphrase: passphrase))
+      end
     end
 
     private
 
     def passphrase
       @passphrase ||= ENV['PASSPHRASE'].to_s.freeze
+    end
+
+    def with_timeout_retry(counter: 0, limit: 3)
+      yield
+    rescue Faraday::TimeoutError, Faraday::ConnectionFailed => error
+      raise if (counter += 1) > limit
+
+      Rails.logger.warn("#{counter} attempt fail due to #{error}")
+      retry
     end
   end
 end
